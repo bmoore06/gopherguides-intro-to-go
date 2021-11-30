a88c4df3dd609df27a861e04a6479d12930e975d
Benjamin Moore <74261880+bmoore06@users.noreply.github.com>
2021-11-30 09:32:04 -0500

    Add files via upload

--- week08/manager_test.go
+++ week08/manager_test.go
@@ -1,1 +1,50 @@
+package week08
 
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Manager_Demonstration(t *testing.T) {
+	t.Parallel()
+
+	r := require.New(t)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	m := &Manager{}
+
+	ctx, err := m.Start(ctx, 5)
+	r.NoError(err)
+
+	for i := 0; i < 10; i++ {
+		go m.Assign(ProductA)
+		go m.Assign(ProductB)
+	}
+
+	var completed []CompletedProduct
+
+	go func() {
+		fmt.Println("waiting for a completed product")
+
+		for cp := range m.Completed() {
+			completed = append(completed, cp)
+
+			if len(completed) >= 20 {
+				m.Stop()
+			}
+		}
+	}()
+
+	fmt.Println("waiting for the ctx to be cancelled")
+	<-ctx.Done()
+
+	fmt.Println("validating output")
+	r.Len(completed, 20)
+	fmt.Println("validated")
+}
